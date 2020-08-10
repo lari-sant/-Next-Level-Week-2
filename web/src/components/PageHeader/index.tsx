a6862b0d4f123fc84c828cfdaa0bdcc4b043d86d
lari-sant <teixeiralary99@gmail.com>
2020-08-09 22:28:30 -0300

    update

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import backIcon from '../../assets/images/icons/back.svg';
-import logoImg from '../../assets/images/logo.svg';
+
+import backIcon from '../../assets/images/icons/back.svg'
+import logoImg from '../../assets/images/logo.svg'
+
 import './styles.css';
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
-  const { title } = props;
   return (
     <header className="page-header">
-      <div className="top-bar-container">
-        <Link to="/">
-          <img src={backIcon} alt="Voltar" />
-        </Link>
-        <img src={logoImg} alt="Logo Proffy" />
-      </div>
-      <div className="header-content">
-        <strong>{title}</strong>
-        {props.children}
-      </div>
-    </header>
-  );
+        <div className="top-bar-container">
+          <Link to='/'>
+            <img src={backIcon} alt="Voltar"/>
+          </Link>
+          <img src={logoImg} alt="Proffy"/>
+        </div>
+
+        <div className="header-content">
+          <strong>{props.title}</strong>
+          { props.description && <p>{props.description}</p> }
+          
+          {props.children}
+        </div>
+
+      </header>
+  )
 };
 
 export default PageHeader;
