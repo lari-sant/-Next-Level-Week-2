5c9b223efad1c7510c8af4375556612feca4b79e
lari-sant <teixeiralary99@gmail.com>
2020-08-10 14:20:44 -0300

    finalizando

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes)
+
+
+app.listen(3333, () => {
+    console.log('🚀 Server started on port 3333');
+  });
+
+  // import express from 'express';
+  // import routes from './routes';
+  
+  // const app = express();
+  
+  // app.use(express.json());
+  // app.use(routes)
+  
+  
+  // app.listen(3333);
